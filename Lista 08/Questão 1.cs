b166ae31a3ea5fd4ca952a9cc83c54da49d92088
dantasjess <jessica.dantas.rocha@gmail.com>
2021-01-28 20:43:37 +0000

    Questão 1, lista 08

---
+++ Lista 08/Questão 1.cs
@@ -0,0 +1,13 @@
+using System;
+class MainClass {
+  public static void Main (string[] args) {
+    Console.WriteLine("Digite o nome do estado");
+    string estado = Console.ReadLine();
+    Console.WriteLine("Digite o número de habitantes");
+    double habitantes = double.Parse(Console.ReadLine());
+    Console.WriteLine("Digite a área do estado em km");
+    double area = double.Parse(Console.ReadLine());
+    double densidade = habitantes/area;
+    Console.WriteLine($"A densidade demográfica do {estado} é de {densidade:0.0} hab/Km.");
+  }
+}
