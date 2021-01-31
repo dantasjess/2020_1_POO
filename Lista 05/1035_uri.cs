bc32d95d1d3773154d1bb30c1d3b840814f7aa8a
dantasjess <jessica.dantas.rocha@gmail.com>
2021-01-31 14:00:36 +0000

    Adicionando 1035

---
+++ Lista 05/1035_uri.cs
@@ -0,0 +1,14 @@
+using System;
+class MainClass {
+  public static void Main (string[] args) {
+   string linha = Console.ReadLine();
+   string[] valores = linha.Split();
+   int A = int.Parse(valores[0]);
+   int B = int.Parse(valores[1]);
+   int C = int.Parse(valores[2]);
+   int D = int.Parse(valores[3]);
+   if (B > C && D > A && (C + D) > (A + B) && C > 0 && D > 0 && A % 2 == 0) 
+   Console.WriteLine("Valores aceitos");
+   else Console.WriteLine("Valores nao aceitos");
+  }
+}
